15cfc657aaf1dbdae55fc8fe3b42411298643e2c
Tracie <severson@usna.edu>
2023-10-12 14:12:32 -0400

    draw background

--- game.py
+++ game.py
@@ -1,2 +1,36 @@
 import pygame
-print("helo world")
+import sys
+
+#Initialize pygame
+pygame.init()
+
+#screen dimensions
+screen_width = 800
+screen_height = 600
+
+#define colors
+BLUE = (0, 0, 255)
+BROWN = (224, 161, 52)
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Blue Background with Brown Rectangle')
+
+#Main Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #fill screen with blue
+    screen.fill(BLUE)
+
+    #draw the brown rectangle
+    rectangle_height = 200
+    pygame.draw.rect(screen, BROWN, (0, screen_height-rectangle_height, screen_width, rectangle_height))
+
+    #update the display
+    pygame.display.flip()
+
+#quit pygame
+pygame.quit()
